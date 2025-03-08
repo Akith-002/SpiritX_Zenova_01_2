2b25b1a9a568dfdd8621d5ea342553e7c847a02f
Dishma Gangulel <madushankaaldg.22@uom.lk>
2025-03-08 22:04:58 +0530

    Update package-lock.json, modify App component imports, and adjust CSS styles for header and section height

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./Pages/Home/login"; // Adjust the path as needed
-import Signup from "./Pages/Home/Signup"; // Assuming you have a Signup component
-import Home from "./Pages/Home/Home"; // Assuming you have a Home component
-import Spiriter from "./Pages/Spiriter";
+import Login from "./Pages/Home/Login";
+import Signup from "./Pages/Home/Signup";
+import Home from "./Pages/Home/Home";
 
 const App = () => {
   return (
@@ -13,8 +12,6 @@ const App = () => {
           <Route path="/" element={<Home />} /> {/* Home Page */}
           <Route path="/login" element={<Login />} /> {/* Login Page */}
           <Route path="/signup" element={<Signup />} /> {/* Signup Page */}
-          <Route path="/spiriter" element={<Spiriter />} />{" "}
-          {/* Spiriter Page */}
         </Routes>
       </div>
     </Router>
