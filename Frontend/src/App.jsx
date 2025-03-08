0ea4f853e5647cf739394594e46ed116d27d5861
Akith-002 <chandinubaa.22@uom.lk>
2025-03-08 21:35:07 +0530

    backend initialize

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Home from "./Pages/Home/Home";
 import "./App.css";
+import Spiriter from "./Pages/Spiriter";
 
 function App() {
   const [count, setCount] = useState(0);
