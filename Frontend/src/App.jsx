e0df57266a160d1a923b19bace01cdd8c528b371
Akith-002 <chandinubaa.22@uom.lk>
2025-03-09 12:04:15 +0530

    Update package-lock.json and refactor AdminPlayers import in App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,12 +9,11 @@ import Signup from "./Pages/Home/Signup";
 // Admin Pages
 import AdminLogin from "./Pages/admin/AdminLogin";
 import AdminSignup from "./Pages/admin/AdminSignup";
-import Players from "./Pages/admin/Players";
+import AdminPlayers from "./Pages/admin/Players";
 import PlayerStats from "./Pages/admin/PlayerStats";
 import TournamentSummary from "./Pages/admin/TournamentSummary";
 // Spiriter Page (Assuming this is another part of the app)
 import Spiriter from "./Pages/Spiriter";
-
 
 const App = () => {
   return (
@@ -22,7 +21,8 @@ const App = () => {
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} /> {/* Home Page */}
-          <Route path="/dashbord" element={<Dashbord />} /> {/* Dashboard Page */}
+          <Route path="/dashbord" element={<Dashbord />} />
+          {/* Dashboard Page */}
           <Route path="/players" element={<Players />} /> {/* Players Page */}
           {/* User Routes */}
           <Route path="/home" element={<Home />} /> {/* Home Page */}
@@ -33,7 +33,7 @@ const App = () => {
           {/* Admin Login Page */}
           <Route path="/admin/signup" element={<AdminSignup />} />
           {/* Admin Signup Page */}
-          <Route path="/admin/players" element={<Players />} />
+          <Route path="/admin/players" element={<AdminPlayers />} />
           {/* Admin Players Management */}
           <Route path="/admin/player-stats" element={<PlayerStats />} />
           {/* Admin Player Stats */}
